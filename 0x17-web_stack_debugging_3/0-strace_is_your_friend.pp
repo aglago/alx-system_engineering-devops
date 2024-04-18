9d3c745e6ffb233ae1cd2f454de93d44f72b3e08
smaglago <amimanyeaglago@gmail.com>
2024-04-18 17:26:04 +0000

    defering to c19

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# 0-strace_is_your_friend.pp fixing typo
-
-exec { 'fix-apache-error':
-        command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-        provider => 'shell'
-}
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,6 +0,0 @@
-# change the security limits fle to allow more files
-
-exec {'replacement':
-  provider => shell,
-  command  => 'sudo sed -i "s/file 5/file 5120/" /etc/security/limits.conf;sudo sed -i "s/file 4/file 4096/" /etc/security/limits.conf'
-}
