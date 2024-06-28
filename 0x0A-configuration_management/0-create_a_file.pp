46cf5ed8787e6d8081e3e1fa71b631d432f9ee24
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-28 22:34:43 +0000

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-# Creates a file in /tmp.
-
-file { 'school':
-  name    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+file {'/tmp/school':
+ensure  => 'present',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
